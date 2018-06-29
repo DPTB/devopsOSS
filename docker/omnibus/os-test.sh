6d9fec9fe7aa80cc5cc45d4861e7a5e7917c1ad5
T <39354485+DPTB@users.noreply.github.com>
2018-06-29 11:41:08 +1000

    Omnibus 8.1.0.5 base install

---
+++ docker/omnibus/os-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "checking processes are running"
+ps -ef | grep obj
+ps -ef | grep nco_p
+echo
+echo "checking Object server is available"
+setarch $(arch) --uname-2.6 /opt/IBM/tivoli/netcool/omnibus/bin/nco_ping NCOMS
+echo
+echo "checking rules file syntax"
+setarch $(arch) --uname-2.6 /opt/IBM/tivoli/netcool/omnibus/probes/nco_p_syntax  -rulesfile linux2x86/simnet.rules  -server NCOMS
